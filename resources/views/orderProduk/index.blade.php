6cecd8b3da17e1a2d60adc4343b126ae8495fc87
HyAnanta <unknownlfjoin@gmail.com>
2024-05-26 17:29:43 +0700

    fix cart

--- resources/views/orderProduk/index.blade.php
+++ resources/views/orderProduk/index.blade.php
@@ -12,96 +12,79 @@
                     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
                     </svg>
-                    <div class="absolute block w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-0.5 start-2.5 dark:border-gray-900"></div>
+                        <div class="absolute block w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-0.5 start-2.5 dark:border-gray-900" id="notifyItemCart" style="opacity: 0;"></div>
+                        <p class="text-xs text-gray-900 hover:text-gray-600 dark:text-white" id="countItemCart"></p>
                 </button>
             </div>
 
-            <!-- Dropdown menu -->
-            <div id="dropdownCart" class="z-20 hidden w-full max-w-sm bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-800 dark:divide-gray-700"  aria-labelledby="dropdownCartButton">
-                <div class="block px-4 py-2 font-medium text-center text-gray-700 rounded-t-lg bg-gray-50 dark:bg-gray-800 dark:text-white">
-                    Cart
-                </div>
-                <div class="block px-4 py-2 font-medium text-center text-gray-700 rounded-t-lg bg-gray-50 dark:bg-gray-800 dark:text-white">
-                <form class="max-w-sm mx-auto">
-                    <select id="member" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option selected hidden>Pilih Member Dahulu</option>
-                        @foreach ($members as $member)
-                        <option value="{{ $member->id }}">{{$member->nama}}</option>
-                        @endforeach
-                    </select>
-                    </form>
-                    <div class="flex justify-between m-2">
-                        <span id="dp_limit">DP limit: -</span>
-                        <span id="rental_total">Rental Total: -</span>
-                        <span id="rental_limit">Max: -</span>
-                    </div>
-                </div>
-                <div class="divide-y divide-gray-100 dark:divide-gray-700" style="max-height: 45vh; overflow-y: auto; z-index: 1;">
-                <!-- dropdownContent -->
-                    <div class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <div class="flex-shrink-0">
-                            <img class="rounded-lg w-11 h-11" src="{{ asset('storage/img/produk/logo.png') }}" alt="Jese image">
-                        </div>
-                        <div class="w-full ps-3">
-    <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400 mx-1 flex items-center space-x-2">
-        <span class="text-gray-800 flex-grow-[2]">Rp 100.000,00</span>
-        <button type="button" class="flex justify-center items-center text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-300 dark:focus:ring-gray-800 p-1 flex-grow">
-            <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14"/>
-            </svg>
-        </button>
-        <span class="text-gray-800">10</span>
-        <button type="button" class="flex justify-center items-center text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-300 dark:focus:ring-gray-800 p-1 flex-grow">
-            <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
-            </svg>
-        </button>
-    </div>
-    <div class="text-xs text-gray-600 dark:text-gray-500 mx-1">Rp 10.000,00</div>
-</div>
-
-                    </div>
-                </div>
-                <div class="block py-2 text-sm font-medium text-center text-gray-900 rounded-b-lg bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white">
-                    <div class="flex justify-between m-2">
-                        <div class="flex items-center">
-                            <label for="default-checkbox" class="text-sm font-medium text-gray-900 dark:text-gray-300">DP: </label>
-                            <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-400 border-gray-500 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 mx-1">
-                            <label for="default-checkbox" class="text-sm font-medium text-gray-900 dark:text-gray-300">Rp 25.000.000,00</label>
-                        </div>
-                        <span id="r" class="text-green-600">Total: Rp 50.000.000,00</span>
-                    </div>
-                </div>
-                <div class="flex">
-                    <!-- Tombol kiri -->
-                    <button class="w-1/3 block py-2 text-sm font-medium text-center text-gray-900 rounded-b-lg bg-red-300 hover:bg-red-400 dark:bg-red-800 dark:hover:bg-red-700 dark:text-white">
-                        <div class="inline-flex items-center justify-center w-full h-full">
-                            <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
-                            </svg>
-                            Hapus Semua
-                        </div>
-                    </button>
-
-                    <!-- Tombol kanan -->
-                    <button class="w-2/3 block py-2 text-sm font-medium text-center text-gray-900 rounded-b-lg bg-green-300 hover:bg-green-400 dark:bg-green-800 dark:hover:bg-green-700 dark:text-white">
-                        <div class="inline-flex items-center justify-center w-full h-full">
-                            <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M8 7V6a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1M3 18v-7a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
-                            </svg>
-                            Transaksi
-                        </div>
-                    </button>
-                </div>
-
-            </div>
+            
         </div>
     </x-slot>
 
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6" x-data="dataProduks">
+    <!-- Dropdown menu -->
+    <div id="dropdownCart" class="z-20 hidden w-full max-w-sm bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-800 dark:divide-gray-700"  aria-labelledby="dropdownCartButton">
+        <div class="block px-4 py-2 font-medium text-center text-gray-700 rounded-t-lg bg-gray-50 dark:bg-gray-800 dark:text-white">
+            Cart
+        </div>
+        <div class="block px-4 py-2 font-medium text-center text-gray-700 rounded-t-lg bg-gray-50 dark:bg-gray-800 dark:text-white">
+        <form class="max-w-sm mx-auto">
+            <select id="member" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected hidden>Pilih Member Dahulu</option>
+                @foreach ($members as $member)
+                <option value="{{ $member->id }}">{{$member->nama}}</option>
+                @endforeach
+            </select>
+            </form>
+            <div class="flex justify-between m-2">
+                <span id="dp_limit">DP limit: -</span>
+                <span id="rental_total">Rental Total: -</span>
+                <span id="rental_limit">Max: -</span>
+            </div>
+        </div>
+        <div class="divide-y divide-gray-100 dark:divide-gray-700" style="max-height: 45vh; overflow-y: auto; z-index: 1;" >
+            <!-- dropdownContent -->
+            <div id="itemCart" x-data="$store.cart">
+                
+            </div>
+        </div>
+        <div class="block py-2 text-sm font-medium text-center text-gray-900 rounded-b-lg bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white">
+            <div class="flex justify-between m-2">
+                <div class="flex items-center">
+                    <label for="default-checkbox" class="text-sm font-medium text-gray-900 dark:text-gray-300">DP: </label>
+                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-400 border-gray-500 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 mx-1">
+                    <label for="default-checkbox" class="text-sm font-medium text-gray-900 dark:text-gray-300">Rp 25.000.000,00</label>
+                </div>
+                <span id="r" class="text-green-600">Total: Rp 50.000.000,00</span>
+            </div>
+        </div>
+        <div class="flex">
+            <!-- Tombol kiri -->
+            <button class="w-1/3 block py-2 text-sm font-medium text-center text-gray-900 rounded-b-lg bg-red-300 hover:bg-red-400 dark:bg-red-800 dark:hover:bg-red-700 dark:text-white">
+                <div class="inline-flex items-center justify-center w-full h-full">
+                    <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+                    </svg>
+                    Hapus Semua
+                </div>
+            </button>
+
+            <!-- Tombol kanan -->
+            <button class="w-2/3 block py-2 text-sm font-medium text-center text-gray-900 rounded-b-lg bg-green-300 hover:bg-green-400 dark:bg-green-800 dark:hover:bg-green-700 dark:text-white">
+                <div class="inline-flex items-center justify-center w-full h-full">
+                    <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M8 7V6a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1M3 18v-7a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
+                    </svg>
+                    Transaksi
+                </div>
+            </button>
+        </div>
+    </div>
+
+    <!-- content -->
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6" x-data="$store.cart">
                 
                     @foreach ($produks as $produk)
                     
@@ -232,12 +215,69 @@
 </script>
 <script>
     document.addEventListener('alpine:init', () => {
-        Alpine.data('dataProduks', () => ({
-                item: [],
-                addToCart($stok){
-                    this.item.push($stok);
-                    console.log(this.item);
+        Alpine.store('cart', () => ({
+            items: [],
+            qty : 0,
+            hargaTotal : 0,
+            addToCart(newItem){
+                this.items.push(newItem);
+                this.qty++;
+                this.hargaTotal += parseFloat(newItem.harga);
+
+                if(this.qty == 0){
+
+                    document.getElementById("notifyItemCart").style.opacity = "0";
+                    document.getElementById("countItemCart").textContent = "";
                 }
-            }));
+                else{
+                    document.getElementById("notifyItemCart").style.opacity = "1";
+                    document.getElementById("countItemCart").textContent = this.qty;
+
+                }
+                console.log(this.items, this.qty);
+                displayItems(this.items);   
+            },
+            cek(){
+                console.log("hehe");
+            },
+        }));
+    });
+</script>
+
+<script>
+    function displayItems(items) {
+        const container = document.getElementById('itemCart');
+        container.innerHTML = ''; // Clear previous content if any
+
+        items.forEach(item => {
+            const itemElement = document.createElement('div');
+            itemElement.classList.add('flex', 'px-4', 'py-3', 'hover:bg-gray-100', 'dark:hover:bg-gray-700');
+
+            itemElement.innerHTML = `
+                <div class="flex-shrink-0">
+                    <img class="rounded-lg w-11 h-11" src="<?= asset('storage/') ?>/${item.gambarProduk}" alt="Item image">
+                    <span>${item.id}</span>
+                </div>
+                <div class="w-full ps-3">
+                    <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400 mx-1 flex items-center space-x-2">
+                        <span class="text-gray-800 flex-grow-[2]">Rp ${item.harga.toLocaleString('id-ID')}</span>
+                        <button type="button" class="flex justify-center items-center text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-300 dark:focus:ring-gray-800 p-1 flex-grow @clik="cek()">
+                            <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" @click="cek()">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14"/>
+                            </svg>
+                        </button>
+                        <span class="text-gray-800">${item.stok}</span>
+                        <button type="button" class="flex justify-center items-center text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-300 dark:focus:ring-gray-800 p-1 flex-grow">
+                            <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="text-xs text-gray-600 dark:text-gray-500 mx-1">Ukuran: ${item.ukuran}</div>
+                </div>
+            `;
+
+            container.appendChild(itemElement);
         });
+    }
 </script>
